b255522f769d099b1afee32fd8cf801fdffbdfe1
Ricardo Valenzuela <rvalenzuelaferrada@gmail.com>
2022-07-10 12:36:28 -0400

    Up vite.config work with homestead

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+let host = 'localhost'
+
 export default defineConfig({
+    server: {
+        hmr: { host },
+        host,
+    },    
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -15,5 +21,16 @@ export default defineConfig({
                 },
             },
         }),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    })
+                }
+            }
+        }           
     ],
 });
