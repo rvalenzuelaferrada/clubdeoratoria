005565a600038d84e29aae7bc7a5b0a960d8d409
Ricardo Valenzuela <rvalenzuelaferrada@gmail.com>
2022-07-22 17:04:06 -0400

    Upgrade vite2 to vite3

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-let host = 'localhost'
-
-export default defineConfig({
-    server: {
-        hmr: { host },
-        host,
-    },    
+export default defineConfig({  
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            refresh: true,
         }),
         vue({
             template: {
